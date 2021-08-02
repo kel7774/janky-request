91c2cbfedf5f3400f09710b973db3f6fcef357ef
Kelli Landry <landry.kel@gmail.com>
2021-08-01 19:40:00 -0500

    added styles to preview page, added songs component to preview

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 import classes from '../styles/Songs.module.css'
 
 const Songs = ({ index, song, removeSong }) => {
+  const { isAuthenticated } = useAuth0()
   return (
     <div className={classes.songContainer}>
       <p className={classes.individualSongs}><span>💩{song}💩</span></p>
-      <button onClick={() => removeSong(index)}>done</button>
+      {isAuthenticated && <button onClick={() => removeSong(index)}>done</button>}
     </div>
   )
 }
--- src/pages/PreviewPage.js
+++ src/pages/PreviewPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Songs from '../components/Songs'
+import classes from '../styles/PreviewPage.module.css'
 
 const PreviewPage = () => {
   return (
-    <div>
-      <h1>preview</h1>
+    <div className={classes.container}>
+      <Songs />
+      <div className={classes.plug}>
+        <p><span>made by <span>👩🏻‍💻</span> <a href='https://kellilandry.dev' target='_blank' rel='noreferrer'>kelli landry</a> (sup, i need a job).</span></p>
+      </div>
     </div>
   )
 }
